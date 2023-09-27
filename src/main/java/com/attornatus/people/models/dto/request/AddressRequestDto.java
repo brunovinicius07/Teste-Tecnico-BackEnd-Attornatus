34c94785c636094a7549ef829c9d1a646e093943
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-09-27 18:42:10 -0300

    Created the PeopleRequest and AddressRequest dtos

---
+++ src/main/java/com/attornatus/people/models/dto/request/AddressRequestDto.java
@@ -0,0 +1,23 @@
+package com.attornatus.people.models.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressRequestDto {
+
+    private String publicPlace;
+
+    private String zipCode;
+
+    private String number;
+
+    private String city;
+
+    private Long idPerson;
+
+    private boolean mainAddress;
+}
---
+++ src/main/java/com/attornatus/people/models/dto/request/PeopleRequestDto.java
@@ -0,0 +1,17 @@
+package com.attornatus.people.models.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PeopleRequestDto {
+
+    private String name;
+
+    private LocalDate birthDate;
+}
