ea34d956df638b77c40b7977af81ded29f33588b
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-09-27 18:04:54 -0300

    created repository and added dependencies

---
+++ src/test/java/com/attornatus/people/PeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus.people;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
