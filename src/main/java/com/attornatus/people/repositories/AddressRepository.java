bf9d1d3fd1113bc1a08ffa7f655f75ec5d17de05
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-09-27 18:52:48 -0300

    Created the PeopleRepository and AddresRepository

---
+++ src/main/java/com/attornatus/people/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.attornatus.people.repositories;
+
+import com.attornatus.people.models.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
---
+++ src/main/java/com/attornatus/people/repositories/PeopleRepository.java
@@ -0,0 +1,9 @@
+package com.attornatus.people.repositories;
+
+import com.attornatus.people.models.entity.People;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<People, Long> {
+}
