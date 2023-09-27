9e5a463feca569fe40c2e489229fc38ff863da40
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-09-27 18:35:23 -0300

    Created the People and Address entities and added the annotations

---
+++ src/main/java/com/attornatus/people/models/entity/Address.java
@@ -0,0 +1,43 @@
+package com.attornatus.people.models.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_address")
+public class Address implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAddress;
+
+    @NotBlank
+    private String publicPlace;
+
+    @NotBlank
+    private String zipCode;
+
+    @NotBlank
+    private String number;
+
+    @NotBlank
+    private String city;
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "id_people")
+    @NotNull
+    private People people;
+
+    @NotNull
+    private boolean mainAddress;
+}
---
+++ src/main/java/com/attornatus/people/models/entity/People.java
@@ -0,0 +1,37 @@
+package com.attornatus.people.models.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_people")
+public class People  implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPerson;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @Past
+    private LocalDate birthDate;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "person", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Address> addresses = new ArrayList<>();
+}
