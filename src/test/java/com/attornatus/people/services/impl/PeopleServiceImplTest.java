1ce3e1a932d203bcde2c45c308af799ec36c8538
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-09-28 23:08:54 -0300

    Created test for the getAllPeople method

--- src/test/java/com/attornatus/people/services/impl/PeopleServiceImplTest.java
+++ src/test/java/com/attornatus/people/services/impl/PeopleServiceImplTest.java
@@ -1,6 +1,5 @@
 package com.attornatus.people.services.impl;
 
-import com.attornatus.people.models.dto.request.PeopleRequestDto;
 import com.attornatus.people.models.dto.response.PeopleResponseDto;
 import com.attornatus.people.models.entity.Address;
 import com.attornatus.people.models.entity.People;
@@ -9,21 +8,18 @@ import com.attornatus.people.repositories.PeopleRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.yaml.snakeyaml.events.Event;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -33,6 +29,7 @@ class PeopleServiceImplTest {
     public static final String NAME             = "João";
     public static final LocalDate BIRTH_DATE    = LocalDate.of(1997, 7, 15);
     public static final List<Address> ADDRESSES = new ArrayList<>();
+    public static final int INDEX               = 0;
 
     @Autowired
     private PeopleServiceImpl peopleServiceImpl;
@@ -58,7 +55,22 @@ class PeopleServiceImplTest {
     }
 
     @Test
-    void getAllPeople() {
+    void whenFindAllThenReturnAnListOfPeople(){
+        when(peopleRepository.findAll()).thenReturn(List.of(people));
+        when(peopleMapper.toPeopleResponseDto(Mockito.any())).thenReturn(peopleResponseDto);
+
+        List<PeopleResponseDto> response = peopleServiceImpl.getAllPeople();
+
+
+        assertNotNull(response);
+        assertEquals(1, response.size());
+        assertEquals(PeopleResponseDto.class, response.get(INDEX).getClass());
+
+        assertEquals(ID, response.get(INDEX).getIdPeople());
+        assertEquals(NAME, response.get(INDEX).getName());
+        assertEquals(BIRTH_DATE, response.get(INDEX).getBirthDate());
+        assertEquals(ADDRESSES, response.get(INDEX).getAddresses());
+
     }
 
     @Test
@@ -72,12 +84,12 @@ class PeopleServiceImplTest {
 
         PeopleResponseDto response = peopleServiceImpl.getPeopleById(ID);
 
-        Assertions.assertNotNull(response);
-        Assertions.assertEquals(PeopleResponseDto.class, response.getClass());
-        Assertions.assertEquals(ID, response.getIdPeople());
-        Assertions.assertEquals(NAME, response.getName());
-        Assertions.assertEquals(BIRTH_DATE, response.getBirthDate());
-        Assertions.assertEquals(ADDRESSES, response.getAddresses());
+        assertNotNull(response);
+        assertEquals(PeopleResponseDto.class, response.getClass());
+        assertEquals(ID, response.getIdPeople());
+        assertEquals(NAME, response.getName());
+        assertEquals(BIRTH_DATE, response.getBirthDate());
+        assertEquals(ADDRESSES, response.getAddresses());
     }
 
     @Test
@@ -88,8 +100,8 @@ class PeopleServiceImplTest {
         try{
             peopleServiceImpl.getPeopleById(ID);
         } catch (Exception ex){
-            Assertions.assertEquals(RuntimeException.class, ex.getClass());
-            Assertions.assertEquals(errorMessage, ex.getMessage());
+            assertEquals(RuntimeException.class, ex.getClass());
+            assertEquals(errorMessage, ex.getMessage());
         }
     }
 
